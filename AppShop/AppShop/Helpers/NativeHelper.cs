7284f6ca8f2efcb4d0535db94079cbd079faa5b3
Lks <nbvf01@list.ru>
2024-06-14 18:07:10 +0300

    Reapply "Добавьте файлы проекта."
    This reverts commit 176d6b8e3f9bf9f57e37452ef318982cc4e89f92.

---
+++ AppShop/AppShop/Helpers/NativeHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using Xamarin.Forms.PlatformConfiguration;
+
+namespace AppShop.Helpers
+{
+    internal class NativeHelper : INativeHelper
+    {
+        public void CloseApp()
+        {
+            Process.GetCurrentProcess().CloseMainWindow();
+            Process.GetCurrentProcess().Close();
+        }
+    }
+}
---
+++ AppShop/AppShop/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppShop
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private bool CanClick = true;
+        private async void ImageButton_Clicked(object sender, EventArgs e)
+        {
+          if (CanClick == false) { return; }
+          CanClick = false;
+          ImageButton btn = ( ImageButton)sender;   
+          await btn.AnimateButton();
+            CanClick = true;
+        }
+
+        private async void ImageButton_Clicked_1(object sender, EventArgs e)
+        {
+            if (!CanClick) { return; }
+            CanClick = false;
+            ImageButton btn = (ImageButton)sender;
+            btn.BackgroundColor = Color.FromHex("E0E0E0");
+            double CurrectScale = btn.Scale;
+
+            await btn.RelScaleTo(-0.10, 150, Easing.SpringIn);
+            await btn.RelScaleTo(+0.15, 250, Easing.SpringOut);
+            await btn.ScaleTo(CurrectScale, 300);
+            CanClick = true;
+        }
+    }
+}
