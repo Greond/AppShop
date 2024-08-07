5c1f458318c3900ed935447aa43348682f9a3983
Lks <nbvf01@list.ru>
2024-08-07 12:12:39 +0300

    добавлена выбор темы приложения в найстройках

--- AppShop/AppShop/Pages/SettingPage/SettingPage.xaml.cs
+++ AppShop/AppShop/Pages/SettingPage/SettingPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AppShop.Pages.SettingPage;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,12 @@ namespace AppShop.Views
 		public SettingPage ()
 		{
 			InitializeComponent ();
+		}
+
+		private void ThemeChanged(object sender, CheckedChangedEventArgs e)
+		{
+			ViewModel viewModel = new ViewModel();
+		
 		}
 	}
 }
--- AppShop/AppShop/Pages/SettingPage/ViewModel.cs
+++ AppShop/AppShop/Pages/SettingPage/ViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using MvvmHelpers;
 using System;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Input;
 using Xamarin.Forms;
@@ -9,6 +10,44 @@ namespace AppShop.Pages.SettingPage
 {
     internal class ViewModel : BaseViewModel
     {
+        private bool _PhoneBindingTheme;
+        private bool _LightTheme;
+        private bool _DarkTheme;
+        public ViewModel()
+        {
+            _PhoneBindingTheme = true;
+        }
+        public bool PhoneBindingTheme
+        {
+            get { return _PhoneBindingTheme; }
+            set 
+            { 
+                _PhoneBindingTheme = value; 
+                OnPropertyChanged(nameof(PhoneBindingTheme));
+                ChangeTheme();
+            }
+        }
+        public bool LightTheme
+        {
+            get => _LightTheme;
+            set
+            {
+                _LightTheme = value;
+                OnPropertyChanged(nameof(LightTheme));
+                ChangeTheme();
+            }
+        }
+        public bool DarkTheme
+        {
+            get { return _DarkTheme; }
+            set
+            {
+                _DarkTheme = value;
+                OnPropertyChanged(nameof(DarkTheme));
+                ChangeTheme();
+            }
+        }
+
         private ICommand _MenuClicked;
         public ICommand MenuClicked
         {
@@ -23,6 +62,22 @@ namespace AppShop.Pages.SettingPage
                         Shell.Current.FlyoutIsPresented = true;
 
                     }));
+            }
+        }
+
+        void ChangeTheme()
+        {
+            if (_LightTheme)
+            {
+                Application.Current.UserAppTheme = OSAppTheme.Light;
+            }
+            else if (_DarkTheme)
+            {
+                Application.Current.UserAppTheme = OSAppTheme.Dark;
+            }
+            else if (_PhoneBindingTheme)
+            {
+                Application.Current.UserAppTheme = OSAppTheme.Unspecified;
             }
         }
     }
